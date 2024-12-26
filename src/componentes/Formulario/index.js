d69549800e274c38c08be13e73cb4ec12ba394ae
Alexandre Teixeira <alextp@gmail.com>
2024-12-25 23:43:51 -0300

    Adição de todos os campos do formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../CampoTexto'; 
+import ListaSuspensa from '../ListaSuspensa'; 
+import Botao from '../Botao'; 
 import './Formulario.css';
 
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        ' Inovação e Gestão'
+        ]
+
     return (
         <section>
             <h2>Preencha os dados para criar o card do colaborador:</h2>
@@ -9,6 +21,10 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome completo" idcampo="nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" idcampo="cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço completo da imagem" idcampo="imagem" />
+            <ListaSuspensa label="Qual é o seu time?" idcampo="times" itens={times} />    
+            <Botao >
+                Criar card
+                </Botao>
             </form>
         </section>
     )
