cb370ea23b5378131aea5f572bffbf0f40ca021e
Alexandre Teixeira <alextp@gmail.com>
2025-01-10 00:26:07 -0300

    correções de bugs

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -2,9 +2,12 @@ import './Banner.css'
 
 function Banner() {
 return (
+    <> 
     <header className="banner">
     <img src="/imagens/banner.png" alt="Banner principal do organo" />
     </header>
+    <h1>Organo</h1>
+    </>
 )
 }
 
